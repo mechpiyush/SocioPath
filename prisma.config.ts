c3ce2eeeea1fd5258d75464a2e594973a4805ca4
mechpiyush <mechpiyush21@gmail.com>
2026-07-19 20:46:49 +0530

    fix: remove earlyAccess and studio from prisma.config.ts to resolve ts error

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from '@prisma/config';
 import process from 'process';
 
 export default defineConfig({
-  earlyAccess: true,
-  studio: {
-    port: 5555,
-  },
+
 
   datasource: {
     url: process.env.DATABASE_URL,
